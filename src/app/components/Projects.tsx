4168ede9c946bf7fa71ba51aab318fb3b12b9add
KashifImran227 <164788718+KashifImran227@users.noreply.github.com>
2024-10-18 11:58:43 +0500

    Update Projects.tsx

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -23,15 +23,15 @@ const projectsData = [
 
 const Projects = () => {
   return (
-    <main className="h-screen flex flex-col items-center bg-gradient-to-b from-[#08090A] via-[#022027] to-[#060606] p-6">
+    <main className="min-h-screen flex flex-col items-center bg-gradient-to-b from-[#08090A] via-[#022027] to-[#060606] p-6">
       <h1 className="text-4xl text-white text-center mb-10 border border-green-200 p-2 shadow-md shadow-green-500 rounded-xl">
         Projects
       </h1>
-      <div className="flex flex-wrap justify-center gap-8 w-full max-w-6xl">
+      <div className="flex flex-wrap justify-center gap-6 w-full max-w-6xl">
         {projectsData.map((project, index) => (
           <div
             key={index}
-            className="flex flex-col w-full sm:w-[45%] md:w-[30%] lg:w-[25%] bg-[#00696d4f] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-[#007B7D] hover:shadow-2xl"
+            className="flex flex-col w-full sm:w-[45%] md:w-[30%] lg:w-[20%] bg-[#00696d4f] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-[#007B7D] hover:shadow-2xl"
           >
             <img
               className="w-full h-48 object-cover transition-transform duration-300 ease-in-out hover:scale-110"
@@ -51,4 +51,12 @@ const Projects = () => {
   );
 };
 
-export default Projects;
+const Page = () => {
+  return (
+    <div className="h-[100vh] w-full bg-gradient-to-b from-[#08090A] via-[#012635] to-[#060606]">
+      <Projects />
+    </div>
+  );
+};
+
+export default Page;
